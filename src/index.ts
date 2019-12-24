72bd95abab4ff4f7a489d9ec785838128ed47b23
rickfaf <rickfaf1@gmail.com>
2019-12-24 21:28:11 +0100

    change loader into plugin

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,73 @@
-import loaderUtils from "loader-utils"
-import { basename } from "path"
 import { updateSource } from "./update-source"
 
-export = async function (source: string) {
-  const options = Object.assign(loaderUtils.getOptions(this) || {}, {
-    sourceMap: this.sourceMap,
-    filePath: this.resourcePath,
-    fileName: basename(this.resourcePath)
-  })
+const pluginName = "SassInTemplateString"
 
-  const updated = await updateSource(source)
-  if (!updated)
-    return source
+class SassInTemplateString {
 
-  const { result, cssResultCode, cssSourceCode, cssSyntax } = updated
+  constructor() {}
 
-  // TODO: Save the content of 'cssSourceCode' somewhere
+  apply(compiler: any) {
+    compiler.hooks.thisCompilation.tap(pluginName, (compilation: any) => {
+      console.log("Assets ", compilation.assets)
+      compilation.hooks.buildModule.tap(pluginName,
+        async (module: any) => {
+          if (module.issuer && module.issuer._source) {
+            const file: string = module.issuer._source._name
+            if (file.endsWith(".ts") || file.endsWith(".js")) {
+              if (file.endsWith("App.ts")) {
+                console.log("App.ts source ", module.issuer._source._value)
+              }
+              const source = module.issuer._source._value
+              let resultReturned: string
+              const updated = await updateSource(source)
+              console.log("Updated ", updated)
+              if (updated) {
+                const { result, cssResultCode, cssSourceCode, cssSyntax } = updated
+                resultReturned = result
+              } else {
+                resultReturned = source
+              }
+              module.issuer._source._value = resultReturned
+              // console.log("Mod ", module.issuer._source)
+            }
+          }
+        }
+      )
+    })
 
-  // console.info(`Result: '${result}'`)
-  // console.info(`Updated: '${updated}'`)
-  // console.info(
-  //   `Source file '${options.fileName}': ${updated ? "updated" : "same"}`
-  // )
-  // if (source !== result)
-  //   console.log("==> source:", result)
-  return result
+  }
+
 }
+
+module.exports = SassInTemplateString
+
+
+
+// import loaderUtils from "loader-utils"
+// import { basename } from "path"
+// import { updateSource } from "./update-source"
+
+// export = async function (source: string) {
+//   const options = Object.assign(loaderUtils.getOptions(this) || {}, {
+//     sourceMap: this.sourceMap,
+//     filePath: this.resourcePath,
+//     fileName: basename(this.resourcePath)
+//   })
+
+//   const updated = await updateSource(source)
+//   if (!updated)
+//     return source
+
+//   const { result, cssResultCode, cssSourceCode, cssSyntax } = updated
+
+//   // TODO: Save the content of 'cssSourceCode' somewhere
+
+//   // console.info(`Result: '${result}'`)
+//   // console.info(`Updated: '${updated}'`)
+//   // console.info(
+//   //   `Source file '${options.fileName}': ${updated ? "updated" : "same"}`
+//   // )
+//   // if (source !== result)
+//   //   console.log("==> source:", result)
+//   return result
+// }
--- src/sass-compiler.spec.ts
+++ src/sass-compiler.spec.ts
@@ -1,27 +1,25 @@
 import { FoundTemplate } from "./find-sass-template"
-import { sassCompiler } from "./sass-compiler"
-import { UpdateSourceOptions } from "./update-source"
+import { compileSassCode } from "./sass-compiler"
 
 
 describe("Tests of 'sassCompiler'", () => {
-  const options: UpdateSourceOptions = {
-    outputFilePath: "extracted/style.css",
-    fileName: "file1.js",
-    filePath: "c/file1.js"
-  }
+  // const options: UpdateSourceOptions = {
+  //   outputFilePath: "extracted/style.css",
+  //   fileName: "file1.js",
+  //   filePath: "c/file1.js"
+  // }
   const foundTemplate: FoundTemplate = {
     start: 16,
     end: 105,
     code: "const template = scss`h1 {\n  font-sizee: 40px;\n  code {\n    font-face: Roboto Mono;\n  }\n}`",
-    varName: "template",
-    stringType: "sass",
+    tagName: "sass",
     value: "h1 {\n  font-sizee: 40px;\n  code {\n    font-face: Roboto Mono;\n  }\n}"
   }
   test("With ", async () => {
 
-    const result = await sassCompiler(foundTemplate, options, true)
+    const result = await compileSassCode(foundTemplate)
 
-    expect(result).toBe(true)
+    expect(result).toBeTruthy()
   })
 
 })
