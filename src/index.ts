6ff1ba50dfc256b8999aa47c500d1c5094697c90
rickfaf <rickfaf1@gmail.com>
2020-01-04 16:18:30 +0100

    extract all files css code in one bundle file

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,54 @@
+import { appendFile, exists, unlink } from "fs-extra"
 import { sassInTemplateStringLoader } from "./loader"
 
 const pluginName = "SassInTemplateString"
 
+interface Options {
+  outputFilePath: string
+}
+
 export default class SassInTemplateString {
 
   static loader = sassInTemplateStringLoader
+  outputFilePath: string
 
-  constructor() {}
+  constructor(options: Options) {
+    this.outputFilePath = options.outputFilePath
+  }
 
   apply(compiler: any) {
     const loader = SassInTemplateString.loader
 
     compiler.hooks.compilation.tap(pluginName, (bundle: any) => {
       bundle.hooks.optimizeModules.tap(pluginName, (modules: any) => {
+        if (!this.outputFilePath)
+          throw new Error("No output file path provided !")
+        exists(this.outputFilePath, exists => {
+          if (exists) {
+            unlink(this.outputFilePath)
+          }
+        })
+        // console.log("Output ", this.outputFilePath)
         modules.forEach(async (mod: any) => {
           // mod._source._value = mod._source._value + "dFFF"
-          const result = await loader(mod._source._value)
-          mod._source._value = result
+          const resultReturned = await loader(mod._source._value)
+          if (typeof resultReturned === "string") {
+            mod._source._value = resultReturned
+          } else {
+            const { result, cssResultCode, cssSourceCode, cssSyntax } = resultReturned
+            mod._source._value = result
+            await appendFile(this.outputFilePath, cssResultCode)
+            // console.log("Sour ", this.cssCode)
+          }
 
           // console.log("Source file ", mod._source._value)
         })
+        // console.log("Sour ", this.cssCode)
       })
+
     })
+
+    // console.log("Sour ", cssCode)
 
   }
 
--- src/loader.ts
+++ src/loader.ts
@@ -1,11 +1,9 @@
-import { updateSource } from "./update-source"
+import { updateSource, UpdateSourceOutput } from "./update-source"
 
-export async function sassInTemplateStringLoader(source: any) {
+export async function sassInTemplateStringLoader(source: any): Promise<string | UpdateSourceOutput> {
   const updated = await updateSource(source)
   if (!updated)
     return source
 
-  const { result, cssResultCode, cssSourceCode, cssSyntax } = updated
-
-  return result
+  return updated
 }
