2622495bb878ca36f6f7a7e39cc6aea5310964de
rickfaf <rickfaf1@gmail.com>
2020-01-04 14:38:28 +0100

    Work on plugin: change file source code

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,28 @@
 import { updateSource } from "./update-source"
 
-const pluginName = "SassInTemplateString"
+const pluginName = "SassInTemplateStringPlugin"
 
-class SassInTemplateString {
+export default class SassInTemplateStringPlugin {
 
   constructor() {}
 
   apply(compiler: any) {
-    compiler.hooks.thisCompilation.tap(pluginName, (compilation: any) => {
-      console.log("Assets ", compilation.assets)
-      compilation.hooks.buildModule.tap(pluginName,
-        async (module: any) => {
-          if (module.issuer && module.issuer._source) {
-            const file: string = module.issuer._source._name
-            if (file.endsWith(".ts") || file.endsWith(".js")) {
-              if (file.endsWith("App.ts")) {
-                console.log("App.ts source ", module.issuer._source._value)
-              }
-              const source = module.issuer._source._value
-              let resultReturned: string
-              const updated = await updateSource(source)
-              console.log("Updated ", updated)
-              if (updated) {
-                const { result, cssResultCode, cssSourceCode, cssSyntax } = updated
-                resultReturned = result
-              } else {
-                resultReturned = source
-              }
-              module.issuer._source._value = resultReturned
-              // console.log("Mod ", module.issuer._source)
-            }
+    compiler.hooks.compilation.tap(pluginName, (bundle: any) => {
+      bundle.hooks.optimizeModules.tap(pluginName, (modules: any) => {
+        modules.forEach(async (mod: any) => {
+          // mod._source._value = mod._source._value + "dFFF"
+          const source = mod._source._value
+          const updated = await updateSource(source)
+          if (updated) {
+            const { result, cssResultCode, cssSourceCode, cssSyntax } = updated
+            mod._source._value = result + "dFFF"
           }
-        }
-      )
+
+          // console.log("Source file ", mod._source._value)
+        })
+      })
     })
 
   }
 
 }
-
-module.exports = SassInTemplateString
-
-
-
-// import loaderUtils from "loader-utils"
-// import { basename } from "path"
-// import { updateSource } from "./update-source"
-
-// export = async function (source: string) {
-//   const options = Object.assign(loaderUtils.getOptions(this) || {}, {
-//     sourceMap: this.sourceMap,
-//     filePath: this.resourcePath,
-//     fileName: basename(this.resourcePath)
-//   })
-
-//   const updated = await updateSource(source)
-//   if (!updated)
-//     return source
-
-//   const { result, cssResultCode, cssSourceCode, cssSyntax } = updated
-
-//   // TODO: Save the content of 'cssSourceCode' somewhere
-
-//   // console.info(`Result: '${result}'`)
-//   // console.info(`Updated: '${updated}'`)
-//   // console.info(
-//   //   `Source file '${options.fileName}': ${updated ? "updated" : "same"}`
-//   // )
-//   // if (source !== result)
-//   //   console.log("==> source:", result)
-//   return result
-// }
